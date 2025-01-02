528704f604575dae4ad87dc052e368dd29446e24
dd104 <dabing.hsiao@gmail.com>
2025-01-02 17:50:07 +0800

    Update common.js

--- js/common.js
+++ js/common.js
@@ -84,7 +84,7 @@ const toLocalISOString= (date) => {
     const localDate = new Date(date - date.getTimezoneOffset() * 60000); //offset in milliseconds. Credit https://stackoverflow.com/questions/10830357/javascript-toisostring-ignores-timezone-offset
    
     // Optionally remove second/millisecond if needed
-    localDate.setHours(null);
+    localDate.setHours(0,0,0,0);
     localDate.setMinutes(null);
     localDate.setSeconds(null);
     localDate.setMilliseconds(null);
